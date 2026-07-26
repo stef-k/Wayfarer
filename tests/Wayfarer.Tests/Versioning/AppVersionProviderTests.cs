04e167f27b5a85b3a95ae72ee9d72fdb56eec033
Stef Karyotidis <stef.kariotidis@gmail.com>
2026-07-26 15:30:54 +0300

    Release Wayfarer v1.8.0 (#400)
    * WIP: begin v1.8.0 release (checkpoint)
    
    * chore(release): prepare v1.8.0

--- tests/Wayfarer.Tests/Versioning/AppVersionProviderTests.cs
+++ tests/Wayfarer.Tests/Versioning/AppVersionProviderTests.cs
@@ -13,7 +13,7 @@ public class AppVersionProviderTests
     {
         var provider = new AppVersionProvider();
 
-        provider.Version.Should().Be("1.7.0");
+        provider.Version.Should().Be("1.8.0");
     }
 
     [Fact]
