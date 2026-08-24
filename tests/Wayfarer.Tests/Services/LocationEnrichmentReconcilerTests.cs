8fb2b26bb09da30a1fb691b43b89814770f1459a
Stef Kariotidis <stef.kariotidis@gmail.com>
2026-08-24 20:11:12 +0300

    WIP: cover final reconciliation races (checkpoint; tests failing)

--- tests/Wayfarer.Tests/Services/LocationEnrichmentReconcilerTests.cs
+++ tests/Wayfarer.Tests/Services/LocationEnrichmentReconcilerTests.cs
@@ -14,6 +14,110 @@ namespace Wayfarer.Tests.Services;
 public sealed class LocationEnrichmentReconcilerTests
 {
     [Fact]
+    public async Task PauseCommittedAfterPageLoadCannotLeaveScheduledTrigger()
+    {
+        var services = new ServiceCollection().BuildServiceProvider();
+        var databaseName = Guid.NewGuid().ToString();
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName).Options;
+        await using var db = new ApplicationDbContext(options, services);
+        var workflow = LocationEnrichmentWorkflow.Create("race-user", DateTime.UtcNow);
+        workflow.Start(DateTime.UtcNow);
+        db.Add(workflow);
+        await db.SaveChangesAsync();
+        var triggerKey = LocationEnrichmentScheduler.TriggerKey(workflow.SchedulerId, workflow.Epoch);
+        var liveTriggers = new HashSet<TriggerKey>();
+        var quartz = SchedulerWithExistingProjection([]);
+        quartz.Setup(item => item.AddJob(It.IsAny<IJobDetail>(), false, It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+        quartz.Setup(item => item.ScheduleJob(It.IsAny<ITrigger>(), It.IsAny<CancellationToken>()))
+            .Returns<ITrigger, CancellationToken>(async (trigger, _) =>
+            {
+                await using var command = new ApplicationDbContext(options, services);
+                var current = await command.LocationEnrichmentWorkflows.SingleAsync();
+                current.Pause(DateTime.UtcNow);
+                await command.SaveChangesAsync();
+                liveTriggers.Add(trigger.Key);
+                return DateTimeOffset.UtcNow;
+            });
+        quartz.Setup(item => item.UnscheduleJob(It.IsAny<TriggerKey>(), It.IsAny<CancellationToken>()))
+            .Returns<TriggerKey, CancellationToken>((key, _) => Task.FromResult(liveTriggers.Remove(key)));
+
+        await new LocationEnrichmentReconciler(db, new LocationEnrichmentScheduler(quartz.Object), quartz.Object)
+            .ReconcileAsync();
+
+        Assert.DoesNotContain(triggerKey, liveTriggers);
+    }
+
+    [Fact]
+    public async Task WorkflowCreatedDuringOrphanDeletionIsReprojected()
+    {
+        var services = new ServiceCollection().BuildServiceProvider();
+        var databaseName = Guid.NewGuid().ToString();
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName).Options;
+        await using var db = new ApplicationDbContext(options, services);
+        var schedulerId = Guid.NewGuid();
+        var orphan = LocationEnrichmentScheduler.JobKey(schedulerId);
+        var quartz = SchedulerWithExistingProjection([orphan]);
+        quartz.Setup(item => item.DeleteJob(orphan, It.IsAny<CancellationToken>()))
+            .Returns<JobKey, CancellationToken>(async (_, _) =>
+            {
+                await using var command = new ApplicationDbContext(options, services);
+                var workflow = LocationEnrichmentWorkflow.Create("created-during-delete", DateTime.UtcNow);
+                typeof(LocationEnrichmentWorkflow).GetProperty(nameof(LocationEnrichmentWorkflow.SchedulerId))!
+                    .SetValue(workflow, schedulerId);
+                workflow.Start(DateTime.UtcNow);
+                command.Add(workflow);
+                await command.SaveChangesAsync();
+                return true;
+            });
+
+        await new LocationEnrichmentReconciler(db, new LocationEnrichmentScheduler(quartz.Object), quartz.Object)
+            .ReconcileAsync();
+
+        quartz.Verify(item => item.AddJob(It.Is<IJobDetail>(job => job.Key == orphan), false,
+            It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task TriggerSnapshotIsNotEnumeratedPerWorkflow()
+    {
+        var services = new ServiceCollection().BuildServiceProvider();
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+        await using var db = new ApplicationDbContext(options, services);
+        var triggers = new CountingSet<TriggerKey>();
+        for (var index = 0; index < 1_001; index++)
+        {
+            var workflow = LocationEnrichmentWorkflow.Create($"linear-{index:D4}", DateTime.UtcNow);
+            workflow.Start(DateTime.UtcNow);
+            db.Add(workflow);
+            triggers.Add(LocationEnrichmentScheduler.TriggerKey(workflow.SchedulerId, workflow.Epoch));
+        }
+        await db.SaveChangesAsync();
+        triggers.ResetEnumerationCount();
+        var quartz = new Mock<IScheduler>();
+        quartz.Setup(item => item.GetTriggerKeys(It.IsAny<GroupMatcher<TriggerKey>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(triggers);
+        quartz.Setup(item => item.GetJobKeys(It.IsAny<GroupMatcher<JobKey>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new HashSet<JobKey>());
+        quartz.Setup(item => item.AddJob(It.IsAny<IJobDetail>(), false, It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        await new LocationEnrichmentReconciler(db, new LocationEnrichmentScheduler(quartz.Object), quartz.Object)
+            .ReconcileAsync();
+
+        Assert.InRange(triggers.EnumerationCount, 0, 1);
+    }
+
+    [Fact]
+    public void ReconcilerOwnsPageScopedContextFactory()
+    {
+        var parameter = Assert.Single(typeof(LocationEnrichmentReconciler).GetConstructors()).GetParameters()[0];
+        Assert.Equal(typeof(IDbContextFactory<ApplicationDbContext>), parameter.ParameterType);
+    }
+    [Fact]
     public async Task WorkflowBeyondFirstThousandIsNotDeletedAsAnOrphan()
     {
         var services = new ServiceCollection().BuildServiceProvider();
@@ -155,5 +259,18 @@ public sealed class LocationEnrichmentReconcilerTests
         quartz.Setup(item => item.CheckExists(It.IsAny<JobKey>(), It.IsAny<CancellationToken>())).ReturnsAsync(true);
         quartz.Setup(item => item.CheckExists(It.IsAny<TriggerKey>(), It.IsAny<CancellationToken>())).ReturnsAsync(true);
         return quartz;
+    }
+
+    private sealed class CountingSet<T> : HashSet<T>
+    {
+        public int EnumerationCount { get; private set; }
+
+        public new IEnumerator<T> GetEnumerator()
+        {
+            EnumerationCount++;
+            return base.GetEnumerator();
+        }
+
+        public void ResetEnumerationCount() => EnumerationCount = 0;
     }
 }
