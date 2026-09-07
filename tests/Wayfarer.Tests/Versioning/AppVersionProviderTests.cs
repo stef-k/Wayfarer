e028dcc36d4f0563bf5a23c4fdf35090fb0f48e9
Stef Kariotidis <stef.kariotidis@gmail.com>
2026-09-07 17:33:36 +0300

    chore(release): prepare Wayfarer 1.9.14

--- tests/Wayfarer.Tests/Versioning/AppVersionProviderTests.cs
+++ tests/Wayfarer.Tests/Versioning/AppVersionProviderTests.cs
@@ -14,7 +14,7 @@ public class AppVersionProviderTests
     {
         var provider = new AppVersionProvider();
 
-        provider.Version.Should().Be("1.9.13");
+        provider.Version.Should().Be("1.9.14");
     }
 
     [Fact]
